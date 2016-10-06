d196ac4150919cb75bd3f2e93fce36e6e4e0899f
Trevor Kimenye <kimenye@gmail.com>
2016-10-06 17:03:48 +0300

    Fixed notify for player

--- data/models/player.js
+++ data/models/player.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Bet = require('./bet.js');
-var notify = require('../../util/notification.js');
+// var notify = require('../../util/notification.js');
 var replies = require('../../behaviours/replies.js');
 var Schema = mongoose.Schema;
 
@@ -59,7 +59,7 @@ playerSchema.methods.progress = function() {
         // progress the user to level 1
         self.level = "1";
         self.save();
-        notify.send(self, replies.texts.leveledUp.replace(/{{level}}/i, "1"));
+        // notify.send(self, replies.texts.leveledUp.replace(/{{level}}/i, "1"));
         break;
       case "1":
         // progress the user to level 1
@@ -70,7 +70,7 @@ playerSchema.methods.progress = function() {
               // only move to level two if there are at least 3 bets
               self.level = "2";
               self.save();
-              notify.send(self, replies.texts.leveledUp.replace(/{{level}}/i, "2"));
+              // notify.send(self, replies.texts.leveledUp.replace(/{{level}}/i, "2"));
             }
           })
 
