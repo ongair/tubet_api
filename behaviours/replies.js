fd0e5101a1cf32b5e1790b83864395ff6931117f
Trevor Kimenye <kimenye@gmail.com>
2016-10-19 06:41:36 +0300

    Added psg correct

--- behaviours/replies.js
+++ behaviours/replies.js
@@ -139,7 +139,7 @@ module.exports = {
     stoke: { full: 'Stoke City', short: 'STK'},
     burnley: { full: 'Burnley', short: 'BUN'},
     ludogorets: { full: 'Ludogorets Razgrad', short: 'LUD'},
-    psg: { full: 'Paris St Germain', 'PSG'},
+    psg: { full: 'Paris St Germain', short: 'PSG'},
     kenya: { full: 'Kenya', short: 'KEN' },
     congo: { full: 'Congo', short: 'DRC'},
     italy: { full: 'Italy', short: 'ITA' },
